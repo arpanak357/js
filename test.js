1c4d52ebc4481c092adddd65c7da2c059cb15b70
Arpana Kumari <121675266+arpanak357@users.noreply.github.com>
2024-06-19 07:46:17 +0000

    prev file

---
+++ test.js
@@ -0,0 +1,22 @@
+// console.log("hello")
+
+// "use strict";
+
+// alert(3+3); //not defined
+
+//have readability
+
+//documentation----
+// https://tc39.es/ecma262/
+//mdn
+
+// data types- string, int, boolean, bigint 
+//null is a data type as well as standalone value as well as an object
+//undefined means value is not yet defined
+//symbol- for uniqueness
+//typeof() --- a fn to know type
+
+
+console.log(typeof(null));  //object
+console.log(typeof(undefined));  //undefined
+
